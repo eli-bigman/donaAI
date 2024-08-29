9835cdd3b6906d658f71a3a87ae43ec193ca9b78
eli-bigman <nutsugah81@gmail.com>
2024-08-29 11:41:46 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <section className="h-screen w-screen flex flex-col gap-3 justify-center items-center">
+            <p className="text-3xl">🤣🤣</p>
+            <p className="text-xl font-bold">
+                Bigman Nutsigah Eli. Your woman dey call you o
+            </p>
+        </section>
+    );
+}
+
+export default App;
